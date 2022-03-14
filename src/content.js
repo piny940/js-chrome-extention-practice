04067efc651cd473f26a3444b1eba955fc34ba6a
mikan <83708535+Ansai940@users.noreply.github.com>
2022-03-14 15:10:25 +0900

    Add trying code

--- src/content.js
+++ src/content.js
@@ -1,3 +1,11 @@
-setInterval(() => {
-    location.reload();
-}, 1740000);
+const test = document.querySelector('body');
+console.log(test);
+test.addEventListener('click', function () {
+    console.log('clicked');
+    console.log(chrome.tabs);
+    // chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
+    //     chrome.tabs.executeScript(tabs[0].id, { code: `document.getSelection().toString()` }, (result) => {
+    //         alert(result);
+    //     });
+    // });
+});
